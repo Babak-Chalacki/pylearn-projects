e040456d61aa7809e4c9fc0fc5496ee295ba553f
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-03 00:31:31 +0330

    Add files via upload

---
+++ project 1 - math/js/triangle.js
@@ -0,0 +1,12 @@
+
+let side1 = Number(prompt("enter your side1"))
+let side2 = Number(prompt("enter your side2"))
+let side3 = Number(prompt("enter your side3"))
+
+result = side1 + side2 + side3
+
+if(result === 180){
+    console.log("it is a triangle")
+}else{
+    console.log("it is not triangle");
+}
