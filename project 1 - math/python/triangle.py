fa082f5c02e552d8b2fca3642d1cd420593fc3f2
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-03 00:32:43 +0330

    Add files via upload

---
+++ project 1 - math/python/triangle.py
@@ -0,0 +1,10 @@
+
+side1 = float(input("enter your side1"))
+side2 = float(input("enter your side2"))
+side3 = float(input("enter your side3"))
+
+result =  side1 + side2 + side3
+if result == 180:
+    print("it is a triangle")
+else:
+    print("it is not a triangle")
