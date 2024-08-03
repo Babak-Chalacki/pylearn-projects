ec33e4b94bf1535957ef7438762ea4f47771cb25
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-03 09:55:51 +0330

    Update triangle.php

--- project 1 - math/php/triangle.php
+++ project 1 - math/php/triangle.php
@@ -4,8 +4,8 @@ if (isset($_GET["side1"]) && isset($_GET["side2"]) && isset($_GET["side3"])) {
     $side1 = $_GET["side1"];
     $side2 = $_GET["side2"];
     $side3 = $_GET["side3"];
-    $result = $side1 + $side2 + $side3;
-    if ($result == 180) {
+    $result = $side1 + $side2 > $side3 || $side1 + $side3 > $side2 || $side3 + $side2 > $side1;
+    if ($result) {
         echo "it is triangle";
     } else {
         echo "its not a triangle";
