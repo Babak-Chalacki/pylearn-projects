026d63c6caab0ae83732f086fa547ba55914cb27
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-03 09:53:18 +0330

    Update triangle.py

--- project 1 - math/python/triangle.py
+++ project 1 - math/python/triangle.py
@@ -3,8 +3,8 @@ side1 = float(input("enter your side1"))
 side2 = float(input("enter your side2"))
 side3 = float(input("enter your side3"))
 
-result =  side1 + side2 + side3
-if result == 180:
+result =  side1 + side2 > side3 or side3 + side2 > side1 or side1 + side3 > side2
+if result :
     print("it is a triangle")
 else:
     print("it is not a triangle")
