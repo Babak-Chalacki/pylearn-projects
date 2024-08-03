c5f17c7c02e028c8e8ec260748bcc25733f52770
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-03 09:56:50 +0330

    Update triangle.js

--- project 1 - math/js/triangle.js
+++ project 1 - math/js/triangle.js
@@ -3,7 +3,7 @@ let side1 = Number(prompt("enter your side1"))
 let side2 = Number(prompt("enter your side2"))
 let side3 = Number(prompt("enter your side3"))
 
-result = side1 + side2 + side3
+result = side1 + side2 > side3 || side1 + side3 > side2 || side3 + side2 > side1;
 
 if(result === 180){
     console.log("it is a triangle")
