11e5cc5defd11db58c9e5eebbd346364da490a4b
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-08-30 14:43:04 -0700

    Update Random_Array_Creator.py

--- project 3 - Array/python/Random_Array_Creator.py
+++ project 3 - Array/python/Random_Array_Creator.py
@@ -1,11 +1,15 @@
-myArrays = []
-while True:
-    user_char = input("enter your some word")
-    if user_char in myArrays:
-        print(f"{user_char} is already exist")
-        print("enter other words")
-    else:
-        myArrays.append(user_char)
-    print(myArrays)
+import random
 
-        
+myArrays = []
+num_random_numbers = 10
+
+while len(myArrays) < num_random_numbers:
+    random_number = random.randint(1, 100)
+    
+    if random_number in myArrays:
+        print(f"{random_number} already exists in the array.")
+    else:
+        myArrays.append(random_number)
+        print(f"Added {random_number} to the array.")
+
+print("Final array of random numbers:", myArrays)
