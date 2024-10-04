895324da22e896b8058b7cedb300e8208bc24bf8
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-10-04 12:00:17 +0330

    make add edit remove search and products detail qrcode maker to store project

---
+++ newproject/test.py
@@ -0,0 +1,13 @@
+me = {
+    'name':'babak',
+    'family':'chalaki',
+    'city':'mashad',
+    'job':'programmer',
+    'average':17.22,
+    'email':'babak.chalaki82@gmail.com',
+    'language':['python','JavaScript','PHP','SQL']
+}
+# print(me["language"][2])
+
+text = 'ied iknf jfbn'
+print(text.split(','))
